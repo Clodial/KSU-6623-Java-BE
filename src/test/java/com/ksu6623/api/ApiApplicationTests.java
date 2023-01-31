6d5927437dd4ff0a84d2126d7b35821b35067620
Clodial <maravillamatthew@gmail.com>
2023-01-30 22:28:14 -0500

    first commit

---
+++ src/test/java/com/ksu6623/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksu6623.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
